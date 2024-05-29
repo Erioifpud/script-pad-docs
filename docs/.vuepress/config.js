db92f71a143c89aba3cdf95e8ac75b1052bfa6d8
Erioifpud <erioifpud@zuo.si>
2024-05-29 17:22:31 +0800

    feat: 调整目录结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,13 +5,13 @@ import { viteBundler } from '@vuepress/bundler-vite'
 export default defineUserConfig({
   lang: 'en-US',
 
-  title: 'VuePress',
-  description: 'My first VuePress Site',
+  title: 'Script Pad',
+  description: 'Script Pad 相关文档',
 
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
 
-    navbar: ['/', '/get-started'],
+    navbar: ['/', '/builtin/'],
   }),
 
   bundler: viteBundler(),
